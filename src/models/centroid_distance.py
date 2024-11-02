251995b3015b35c0b4d12fe7dab241983e58de26
Yash Y <yyardi@imsa.edu>
2024-11-02 00:18:30 +0000

    revised centroid

--- src/models/centroid_distance.py
+++ src/models/centroid_distance.py
@@ -2,12 +2,39 @@ import numpy as np
 import torch
 from scipy.spatial.distance import euclidean, cosine
 
-def euclidean_distance(embeddings, dataset_flags):
-    #find normalization value
-    norm = np.mean(np.std(embeddings, axis=0, ddof=1))
-    print("Normalization Value:", norm)
+def min_max(embeddings):
+    # Check for NaNs and Infs
+    if np.any(np.isnan(embeddings)) or np.any(np.isinf(embeddings)):
+        raise ValueError("Input array contains NaNs or Infs. Please clean the data.")
     
-    # Create masks for sim and real data
+    min_vals = embeddings.min(axis=0)
+    max_vals = embeddings.max(axis=0)
+    
+    range_vals = max_vals - min_vals
+    range_vals = np.where(range_vals == 0, 1, range_vals)  # Prevent division by zero
+    
+    normalized_embeddings = (embeddings - min_vals) / range_vals
+    return normalized_embeddings
+
+def std_dev(embeddings):
+    for i in range(len(embeddings)):
+        embeddings[i] = embeddings[i] / np.std(embeddings[i], axis=0, ddof=1)
+    return embeddings
+
+def euclidean_distance(embeddings, dataset_flags, distance_metric, normalization):
+    
+    #Normalize
+    norm = 1
+    if normalization == "std_dev":
+        #norm = np.mean(np.std(embeddings, axis=0, ddof=1))
+        std_dev(embeddings)
+    elif normalization == "mean":
+        norm = np.sqrt(embeddings.shape[1])
+    elif normalization == "min_max":
+        embeddings = min_max(embeddings)
+    print(normalization, "Normalization Value:", norm, "(1 indicates min_max)")
+
+    #Create masks for sim and real data
     sim_mask = dataset_flags == "s"
     real_mask = dataset_flags == "r"
     sim_embeddings = (embeddings[sim_mask])/norm
@@ -20,29 +47,11 @@ def euclidean_distance(embeddings, dataset_flags):
     print("Real Centroid:", real_centroid.shape)
 
     #calculate normalized euclidean distance
-    euclidean_distance = euclidean(sim_centroid, real_centroid)
-    print("Normalized Euclidean Distance:", euclidean_distance)
-    return euclidean_distance
-
-def cosine_distance(embeddings, dataset_flags):
-    #find normalization value
-    norm = np.mean(np.std(embeddings, axis=0, ddof=1))
-    print("Normalization Value:", norm)
-
-    # Create masks for sim and real data
-    sim_mask = dataset_flags == "s"
-    real_mask = dataset_flags == "r"
-    sim_embeddings = embeddings[sim_mask]/norm
-    real_embeddings = embeddings[real_mask]/norm
-
-    #find centroids
-    sim_centroid = np.mean(sim_embeddings, axis=0)
-    real_centroid = np.mean(real_embeddings, axis=0)
-    print("Sim Centroid:", sim_centroid.shape)
-    print("Real Centroid:", real_centroid.shape)
-
-    #calculate normalized euclidean distance
-    cosine_distance = cosine(sim_centroid, real_centroid)
-    print("Normalized Cosine Distance:", cosine_distance)
-    return cosine_distance
-
+    if distance_metric == "euclidean":
+        euclidean_distance = euclidean(sim_centroid, real_centroid)
+        print("Normalized Euclidean Distance:", euclidean_distance)
+        return euclidean_distance
+    elif distance_metric == "cosine":
+        cosine_distance = cosine(sim_centroid, real_centroid)
+        print("Normalized Cosine Distance:", cosine_distance)
+        return cosine_distance
